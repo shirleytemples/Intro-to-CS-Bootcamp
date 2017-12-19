26171cd82da8491108a0f422e0cd040e3cb0b66a
shirleytemples <34287363+shirleytemples@users.noreply.github.com>
2017-12-18 23:59:22 -0800

    create

---
+++ fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts 'What is your first name?'
+first = gets.chomp
+puts 'And what is your middle name?'
+middle = gets.chomp
+puts 'And your last?'
+last = gets.chomp
+puts 'Nice to meet you, ' + first +' ' + middle + ' ' + last+'.'
