b4e5e880bb8681bcc91b1d1d43d199c8505cc42c
shirleytemples <34287363+shirleytemples@users.noreply.github.com>
2017-12-19 00:00:57 -0800

    add

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,3 @@
+puts 'Hello! What is your favorite number?'
+number = gets.chomp
+puts 'Naw, ' + (number.to_i + 1).to_s + ' is bigger and better. That should be your new favorite number! (｡•ˇ‸ˇ•｡).'
