77e0ba8ac393402ff64d01ee01d179d9c94e73e6
shirleytemples <34287363+shirleytemples@users.noreply.github.com>
2017-12-27 20:52:00 -0800

    add

---
+++ ch07/leapYearCounter.rb
@@ -0,0 +1,16 @@
+puts 'leap years program'.center(40)
+puts ''
+puts 'what is the starting year:'
+starting = gets.to_i
+puts 'Please type an ending year:'
+	while (ending = gets.to_i)<starting
+		puts 'ending year should be later than starting year, please re-input:'
+	end
+
+		puts 'All leap years below:'
+		while starting <= ending
+			if (starting%100 != 0 and starting%4 ==0) or (starting%100 == 0 and starting%400== 0)
+				puts starting
+			end
+		starting= starting+1
+		end
