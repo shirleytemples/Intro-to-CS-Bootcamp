de52f6c84a28dfcee5e479c4dabf490184f05b33
shirleytemples <34287363+shirleytemples@users.noreply.github.com>
2018-01-11 21:24:02 -0800

    change

--- ch10/sort.rb
+++ ch10/sort.rb
@@ -1,14 +1,28 @@
-def sort some_array 
-  recursive_sort some_array, []
-end
+array = ['big', 'small', 'Happy', 'little', 'slow', 'fantastic', 'Xenon', 'glorious', 'he', 'Jazz', 'zebra']
+sorted_array = []
 
-def recursive_sort unsorted_array, sorted_array 
-  response= gets.chomp
-
-  while response != ''
-	responses.push response
-	response = gets.chomp 
+def mySort(array, sorted_array)
+  if array.length <= 0
+    return
   end
-
-  puts responses.sort
+  x = 0
+  y = 0
+  while y < array.length 
+    if array[x] < array[y]
+      y += 1
+    elsif array[x] > array[y]
+      x = y
+      y += 1
+    else
+      y += 1
+    end
+  end
+  sorted_array.push(array[x]) 
+  array.delete(array[x])
+  mySort(array, sorted_array)
 end
+mySort(array, sorted_array)
+
+puts sorted_array
+puts
+puts array
